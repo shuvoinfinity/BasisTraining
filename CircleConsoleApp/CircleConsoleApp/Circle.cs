074cac33c1b3ccc4ee9d17c21b53fedd8baaca8f
shuvoinfinity <write2subrata@outlook.com>
2014-09-27 14:40:38 +0600

    Class Date 27/09/2014

---
+++ CircleConsoleApp/CircleConsoleApp/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleConsoleApp
+{
+    class Circle
+    {
+        public double radius;
+
+        //public double GetDiameter(double radius)
+        //{
+        //    return 2*radius;
+        //}
+        public double GetDiameter()
+        {
+            return 2 * radius;
+        }
+
+        //public double GetPerimeter(double radius)
+        //{
+        //    return 2*Math.PI*radius;
+        //}
+        public double GetPerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        //public double GetRadious(double radius)
+        //{
+        //    return Math.PI*radius*radius;
+        //}
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
---
+++ CircleConsoleApp/CircleConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Circle aCircle = new Circle();
+
+            ////double radius = Console.ReadLine().ToString();
+            //double radius = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("\n" + aCircle.GetDiameter(radius) +"\n");
+            //Console.WriteLine(aCircle.GetPerimeter(radius)+ "\n");
+            //Console.WriteLine(aCircle.GetRadious(radius) + "\n");
+
+            Circle aCircle = new Circle();
+            aCircle.radius = 100;
+
+            Console.WriteLine("Perimeter " + aCircle.GetPerimeter());
+            Console.WriteLine("Perimeter " + aCircle.GetDiameter());
+            Console.WriteLine("Perimeter " + aCircle.GetArea());
+
+            Console.ReadKey();
+
+        }
+    }
+}
